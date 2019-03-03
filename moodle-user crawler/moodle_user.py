34724883c3d03277cc3ec9a5809eb28a87bf1407
吳昱宏 <edward.0726@gmail.com>
2019-03-04 07:13:47 +0800

    fix(moodle user crawler): multi class bug

--- moodle-user crawler/moodle_user.py
+++ moodle-user crawler/moodle_user.py
@@ -1,3 +1,8 @@
+# author: 吳昱宏
+# title: Moodle user crawler
+# created at: May 13, 2018
+# last modified at: March 3, 2019
+
 import requests
 import getpass
 import sys
@@ -23,16 +28,31 @@ post_data = {'username': USER, 'password': PASSWORD, 'logintoken': logintoken}
 
 html_post = session.post(LOGIN_URL, post_data)
 
-class_id = str(input('請問您想查詢的課程代碼是：').upper())
+class_id = str(input('請輸入您想查詢的課程代碼或關鍵字：').upper())
 
 soup = BeautifulSoup(html_post.text, 'html.parser')
 
+classes = list() # 不用dict的原因為下面操作會較不方便
 for i in soup.find_all('a', href=True):
     if class_id in str(i.string):
-        print("查詢課程：" , i.string)
-        class_url = i['href']
+        classes.append([i.string, i['href']])
 
-try: 
+if len(classes) == 0:
+    print('您的輸入有誤，或是權限不足！！（若您無選修該堂課，無法查詢）')
+else:
+    if len(classes) == 1:
+        class_name, class_url = classes[0][0], classes[0][1]
+    else:
+        output_str = ''.join(['{}. {}\n'.format(str(index + 1), i[0]) for index, i in enumerate(classes)])
+        try:
+            index = int(input('查到多筆課程，請用數字回答你要搜尋哪一堂課：\n' + output_str))
+        except:
+            print('輸入有誤！')
+        if index > len(classes):
+            print('輸入有誤！')
+        else:
+            class_name, class_url = classes[index - 1][0], classes[index - 1][1]
+    print('查詢課程：' + class_name)
     class_url = class_url.replace("course", "user")
     class_url = class_url.replace("view", "index")
     class_url += "&perpage=5000"
@@ -46,5 +66,3 @@ try:
     user_list.sort()
     print('包含助教共計 {length} 人'.format(length=len(user_list)))
     print(*user_list, sep = '\n')
-except:
-    print('您的輸入有誤，或是權限不足！！（若您無選修該堂課，無法查詢）')
